7ef598d4aedb0ca1a3a9a4c948bcbea8ee6e7526
azkbrqlna <azkbrq@gmail.com>
2025-01-18 13:19:13 +0700

    feat: usePathname in nav items

--- src/components/Navbar/NavItem.jsx
+++ src/components/Navbar/NavItem.jsx
@@ -1,9 +1,15 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function NavItem({ children, href, onClick }) {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+
   return (
     <Link
-      className="font-cera text-sm transition duration-150 ease-in-out hover:-translate-y-1 hover:font-bold"
+      className={`font-cera text-sm transition duration-150 ease-in-out hover:-translate-y-1 hover:font-bold ${
+        isActive ? "font-bold" : "font-normal"
+      }`}
       href={href}
       onClick={onClick}
     >
