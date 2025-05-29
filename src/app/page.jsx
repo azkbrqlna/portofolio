02387906afb7ded7e34c6b14cd261fd6fc83175f
azkbrqlna <azkbrq@gmail.com>
2025-05-29 22:11:32 +0700

    feat: randomize greetings in about and change social media design

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -32,33 +32,8 @@ export default function AboutPage() {
       />
       <div className="pt-24 flex flex-col mt-10 md:flex-row justify-center">
         <div className="relative z-10 flex flex-col md:flex-row items-center md:items-start gap-8 text-center md:text-left px-8">
-          <div className="flex-1 flex justify-center order-1 md:order-2">
-            <FadeText
-              text={
-                <div className="w-48 h-48 rounded-full overflow-hidden">
-                  <Image
-                    src="/images/copy.jpg"
-                    alt="Azka Bariqlana"
-                    width={192}
-                    height={192}
-                    className="object-cover"
-                  />
-                </div>
-              }
-              framerProps={{
-                show: {
-                  transition: {
-                    delay: 1,
-                    duration: 0.8,
-                  },
-                },
-              }}
-              direction="left"
-            />
-          </div>
-
-          <div className="flex-1 max-w-lg order-2 md:order-1">
-            <div className="text-4xl font-bold font-cera ">
+          <div className="flex-1 max-w-lg">
+            <div className="text-3xl font-bold font-cera ">
               <FadeText
                 text={
                   <>
@@ -92,23 +67,72 @@ export default function AboutPage() {
               }}
             />
           </div>
+
+          <div className="flex-1 flex justify-center">
+            <FadeText
+              text={
+                <div className="w-48 h-48 rounded-full overflow-hidden  ">
+                  <Image
+                    src="/images/copy.jpg"
+                    alt="Azka Bariqlana"
+                    width={192}
+                    height={192}
+                  />
+                </div>
+              }
+              framerProps={{
+                show: {
+                  transition: {
+                    delay: 1,
+                    duration: 0.8,
+                  },
+                },
+              }}
+              direction="left"
+            />
+          </div>
         </div>
       </div>
 
       <FadeText
         text={
-          <div className="flex flex-col items-center mt-8">
-            <h2 className="text-md font-semibold">Connect with me:</h2>
-            <div className="flex space-x-1 text-xl">
-              <DockItems href="https://www.linkedin.com/in/azka-bariqlana-06a3482a1/">
-                <FaLinkedin />
-              </DockItems>
-              <DockItems href="https://github.com/azkbrqlna">
-                <FaGithub />
-              </DockItems>
-              <DockItems href="https://www.instagram.com/azkbrqlnaaa_/">
-                <FaInstagram />
-              </DockItems>
+          <>
+            <div className="flex flex-col items-center mt-8 mb-8 ">
+              <h2 className="text-md font-semibold">Connect with me :</h2>
+              <div className="flex justify-center items-center gap-6 mt-8 space-x-1">
+                {/* LinkedIn */}
+                <Link
+                  href="https://www.linkedin.com/in/azka-bariqlana-06a3482a1/"
+                  className="w-16 h-16 flex items-center justify-center bg-white dark:bg-neutral-950 border-2 border-black dark:border-white shadow-dark dark:shadow-light
+               transform rotate-2 hover:rotate-0 hover:scale-110 transition-all duration-300"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaLinkedin className="text-2xl text-black dark:text-white hover:scale-125 transition-transform duration-200" />
+                </Link>
+
+                {/* GitHub */}
+                <Link
+                  href="https://github.com/azkbrqlna"
+                  className="w-16 h-16 flex items-center justify-center bg-white dark:bg-neutral-950 border-2 border-black dark:border-white shadow-dark dark:shadow-light
+               transform -rotate-2 hover:rotate-0 hover:scale-110 transition-all duration-300"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaGithub className="text-2xl text-black dark:text-white hover:scale-125 transition-transform duration-200" />
+                </Link>
+
+                {/* Instagram */}
+                <Link
+                  href="https://www.instagram.com/azkbrqlnaaa_/"
+                  className="w-16 h-16 flex items-center justify-center bg-white dark:bg-neutral-950 border-2 border-black dark:border-white shadow-dark dark:shadow-light
+               transform rotate-1 hover:rotate-0 hover:scale-110 transition-all duration-300"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaInstagram className="text-2xl text-black dark:text-white hover:scale-125 transition-transform duration-200" />
+                </Link>
+              </div>
             </div>
           </>
         }
